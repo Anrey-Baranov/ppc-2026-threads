70f8afe2a406eecd8d8c3f47259a44058c653475
Victoria <vika.kondrashova.05@inbox.ru>
2026-04-07 21:08:21 +0300

    fix1

--- tasks/kondrashova_v_marking_components/tests/functional/main.cpp
+++ tasks/kondrashova_v_marking_components/tests/functional/main.cpp
@@ -8,6 +8,7 @@
 
 #include "kondrashova_v_marking_components/common/include/common.hpp"
 #include "kondrashova_v_marking_components/seq/include/ops_seq.hpp"
+#include "kondrashova_v_marking_components/omp/include/ops_omp.hpp"
 #include "util/include/func_test_util.hpp"
 
 namespace kondrashova_v_marking_components {
