bc50ebf9a1e3755e014dba4e6ef0d72402fc7536
Andrew-8705 <abaldin05@mail.ru>
2026-05-01 11:53:48 +0000

    ALL version of radix sort

---
+++ tasks/baldin_a_radix_sort/all/include/ops_all.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "baldin_a_radix_sort/common/include/common.hpp"
+#include "task/include/task.hpp"
+
+namespace baldin_a_radix_sort {
+
+class BaldinARadixSortALL : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kALL;
+  }
+  explicit BaldinARadixSortALL(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+
+  void DistributeData(int rank, int size, int &n);
+  void GatherData(int rank);
+
+  std::vector<int> counts_;
+  std::vector<int> displs_;
+  std::vector<int> local_data_;
+};
+
+}  // namespace baldin_a_radix_sort
